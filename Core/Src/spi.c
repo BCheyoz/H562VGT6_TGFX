1a4d03a59179328a0d851cbba2dc2fe57463a919
JoëlP <joel.proux@aldes.com>
2025-02-27 21:35:03 +0100

    Après re-Génération CubeMX, TFL4-1460 TFL4-1471 Ref #16 #5 Add Modbus

--- Core/Src/spi.c
+++ Core/Src/spi.c
@@ -265,7 +265,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle)
     PE5     ------> SPI4_MISO
     PE6     ------> SPI4_MOSI
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6;
+    GPIO_InitStruct.Pin = AFF_SPI4_SCK_Pin|AFF_SPI4_CS_Pin|AFF_SPI4_SDO_Pin|AFF_SPI4_SDA_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -337,7 +337,7 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* spiHandle)
     PE5     ------> SPI4_MISO
     PE6     ------> SPI4_MOSI
     */
-    HAL_GPIO_DeInit(GPIOE, GPIO_PIN_2|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6);
+    HAL_GPIO_DeInit(GPIOE, AFF_SPI4_SCK_Pin|AFF_SPI4_CS_Pin|AFF_SPI4_SDO_Pin|AFF_SPI4_SDA_Pin);
 
   /* USER CODE BEGIN SPI4_MspDeInit 1 */
 
