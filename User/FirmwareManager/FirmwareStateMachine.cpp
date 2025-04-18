8fddff7506937e153dcbc9e22525650f5aef4ef3
Boris chhay <boris.chhay@aldes.com>
2025-04-18 17:18:12 +0200

    Fix CC MV command unit

--- User/FirmwareManager/FirmwareStateMachine.cpp
+++ User/FirmwareManager/FirmwareStateMachine.cpp
@@ -585,7 +585,7 @@ void FwMng::CtrlCmdTask(){
 	// maj de la commande *************************************************
 	cc_out = ctrlCmd->getExternalOutputs().Control_Out;
 
-	setFanExhaustVoltage_mV(cc_out.Cs_vent_vltg_sp * 100); // Cs_vent_vltg_sp sortie en Volt x10
+	setFanExhaustVoltage_mV(cc_out.Cs_vent_vltg_sp); // Cs_vent_vltg_sp sortie en milliVolt
 	SetEmbracoInverterSpeedConsRPM(cc_out.Cs_heat_pump_rot_spd_sp);
 
 	if(cc_out.Ss_elec_bstr_htr_sp != te_on_off::off){
