6d76f741ad85eeac3cc58fc427d112352f6f72af
JoëlP <joel.proux@aldes.com>
2025-05-21 18:10:16 +0200

    Lecture de U3 "MX25L6433" (Flash_Spi) sur SPI3 incorrecte avec SPI_NSS_HARD_OUTPUT sur PA15, DATASIZE_8BIT, PRESCALER_8 (30 MBits/s) & GPIO_SPEED_FREQ_VERY_HIGH

--- Core/Src/spi.c
+++ Core/Src/spi.c
@@ -87,7 +87,7 @@ void MX_SPI3_Init(void)
   hspi3.Init.DataSize = SPI_DATASIZE_8BIT;
   hspi3.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi3.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi3.Init.NSS = SPI_NSS_HARD_INPUT;
+  hspi3.Init.NSS = SPI_NSS_HARD_OUTPUT;
   hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
   hspi3.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi3.Init.TIMode = SPI_TIMODE_DISABLE;
