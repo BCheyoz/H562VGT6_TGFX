697f8c64e1355f442c85eebce88e5c1a8ccf4f92
JoëlP <joel.proux@aldes.com>
2025-05-22 10:58:16 +0200

    Lecture de U3 "MX25L6433" (Flash_Spi) sur SPI3 incorrecte avec SPI_NSS_HARD_OUTPUT & GPIO_PULLUP sur PA15, GPIO_SPEED_FREQ_VERY_HIGH, SS_IDLENESS_15CYCLE & INTERDATA_IDLENESS_15CYCLE

--- Core/Src/spi.c
+++ Core/Src/spi.c
@@ -224,7 +224,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle)
     */
     GPIO_InitStruct.Pin = Flash_SPI_CS_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF6_SPI3;
     HAL_GPIO_Init(Flash_SPI_CS_GPIO_Port, &GPIO_InitStruct);
