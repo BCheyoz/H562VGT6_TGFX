f0776e97cfcf75b10c2dc7b22c9e4dfe3c86247e
Boris chhay <boris.chhay@aldes.com>
2025-06-24 09:52:49 +0200

    upgrade to Beta 0.02.0C

--- User/VersionInfos/VersionInfos.h
+++ User/VersionInfos/VersionInfos.h
@@ -26,7 +26,7 @@ extern "C" {
 #define VERSION_FW_SOFT_TYPE    (0xB0)  // Bin type
 #define VERSION_FW_SOFT_MAJOR   (0x00)  // major version
 #define VERSION_FW_SOFT_MINOR   (0x02)  // Minor version
-#define VERSION_FW_SOFT_DEV     (0x0B)  // Dev version
+#define VERSION_FW_SOFT_DEV     (0x0C)  // Dev version
 
 
 #define VERSION_FW_SOFT			VI_MAKE_VER_SOFT_32(VERSION_FW_SOFT_TYPE, VERSION_FW_SOFT_MAJOR, VERSION_FW_SOFT_MINOR, VERSION_FW_SOFT_DEV)
