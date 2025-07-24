af98cc07d6a43e971882c63a8f997131f4f82a51
Boris chhay <boris.chhay@aldes.com>
2025-07-24 10:31:44 +0200

    fix orientation layout

--- TouchGFX/generated/gui_generated/include/gui_generated/common/SimConstants.hpp
+++ TouchGFX/generated/gui_generated/include/gui_generated/common/SimConstants.hpp
@@ -4,8 +4,8 @@
 #ifndef SIMCONSTANTS_HPP
 #define SIMCONSTANTS_HPP
 
-static unsigned short SIM_WIDTH = 240;
-static unsigned short SIM_HEIGHT = 320;
+static unsigned short SIM_WIDTH = 320;
+static unsigned short SIM_HEIGHT = 240;
 #define SIM_TITLE "Tflow4"
 
 #endif // SIMCONSTANTS_HPP
